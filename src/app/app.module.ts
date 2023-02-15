d0138b490e0ec9d8df81305f1c19b2c3fe8ea375
avinash-repo <124034259+avinash-repo@users.noreply.github.com>
2023-02-16 03:17:03 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoFormComponent } from './components/todo-form/todo-form.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { ToastrModule } from 'ngx-toastr';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Route } from '@angular/router';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/components/login/login.component';
 import { RegisterComponent } from './auth/components/register/register.component';
@@ -26,12 +16,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { AuthModule } from './auth/auth.module';
+import { FormsModule } from '@angular/forms';
 import { authInterceptorProviders } from './services/interceptor.service';
 import { AuthGuardService } from './guards/auth-guard.service';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
@@ -44,21 +36,10 @@ import { NzNotificationModule } from 'ng-zorro-antd/notification';
 import { NzProgressModule } from 'ng-zorro-antd/progress';
 import { NzTableModule } from 'ng-zorro-antd/table';
 
-const routes: Route[] = [
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'add', component: TodoFormComponent },
-  { path: 'list', component: TodoListComponent },
-  { path: 'favorite', component: TodoListComponent },
-  { path: '**', component: PageNotFoundComponent },
-];
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodoListComponent,
-    TodoFormComponent,
-    TodoComponent,
-    PageNotFoundComponent,
     AppComponent,
     HomeComponent,
     LoginComponent,
@@ -73,12 +54,6 @@ const routes: Route[] = [
     ProductCardComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    FormsModule,
-    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     IvyCarouselModule,
@@ -98,8 +73,11 @@ const routes: Route[] = [
     NzProgressModule,
     NzTableModule,
   ],
-  providers: [],
+  providers: [
+    authInterceptorProviders,
+    AuthGuardService,
+    { provide: NZ_I18N, useValue: en_US },
+  ],
   bootstrap: [AppComponent],
-  exports: [TodoComponent, TodoListComponent, TodoFormComponent,PageNotFoundComponent,],
 })
 export class AppModule {}
